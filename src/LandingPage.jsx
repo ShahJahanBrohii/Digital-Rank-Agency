d36f2b5c7fd9f36547abe079a36f8c385b607dc9
ShahJahanBro <shahjahan12098@gmail.com>
2026-01-17 16:24:57 +0500

    updated

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import Stats from './components/Stats';
-// import ContactForm from './components/ContactForm';
+import ContactForm from './components/ContactForm';
 // import Footer from './components/Footer';
 // import BackgroundEffects from './components/BackgroundEffects';
 
@@ -16,6 +16,7 @@ const LandingPage = () => {
           <Hero />
           <Services />
           <Stats />
+          <ContactForm />
         </main>
     </div>
   );
